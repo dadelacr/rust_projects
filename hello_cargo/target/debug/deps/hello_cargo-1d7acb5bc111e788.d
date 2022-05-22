6edb5e28c1803bef50bc5dff81c632c857450868
David D <54550428+dadelacr@users.noreply.github.com>
2022-05-21 21:54:26 -0700

    trying this

---
+++ hello_cargo/target/debug/deps/hello_cargo-1d7acb5bc111e788.d
@@ -0,0 +1,5 @@
+/Users/dadelacr/projects/hello_cargo/target/debug/deps/hello_cargo-1d7acb5bc111e788.rmeta: src/main.rs
+
+/Users/dadelacr/projects/hello_cargo/target/debug/deps/hello_cargo-1d7acb5bc111e788.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/dadelacr/projects/hello_cargo/target/debug/hello_cargo: /Users/dadelacr/projects/hello_cargo/src/main.rs
